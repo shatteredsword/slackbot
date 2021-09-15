b1a52ac2993d7da2e70d46acdd0af1442709c806
Sean Mayo <sean.mayo@matsonfruit.com>
2021-09-15 10:54:25 -0700

    added 2nd message for testing

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import logging
 from flask import Flask
@@ -13,23 +14,30 @@ slack_events_adapter = SlackEventAdapter(
 slack_web_client = WebClient(token=os.getenv("SLACKBOT_TOKEN"))
 
 
+# responds to sent messages
 @slack_events_adapter.on("message")
 def message(payload):
     event = payload.get("event", {})
-
-    MESSAGE_BLOCK = {
-        "type": "section",
-        "text": {"type": "mrkdwn", "text": "Howdy! :face_with_cowboy_hat:"},
-    }
-
     text = event.get("text")
 
     if "hello pytexas" in text.lower():
+        MESSAGE_BLOCK = {
+            "type": "section",
+            "text": {"type": "mrkdwn", "text": "Howdy! :face_with_cowboy_hat:"},
+        }
         channel_id = event.get("channel")
-
         message = {"channel": channel_id, "blocks": [MESSAGE_BLOCK]}
-
         slack_web_client.chat_postMessage(**message)
+
+    if "what day is it?" in text.lower():
+        if datetime.datetime.today().weekday() == 2:
+            MESSAGE_BLOCK = {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": "https://i.imgur.com/hy8S3NC.jpg"},
+            }
+            channel_id = event.get("channel")
+            message = {"channel": channel_id, "blocks": [MESSAGE_BLOCK]}
+            slack_web_client.chat_postMessage(**message)
 
 
 if __name__ == "__main__":
