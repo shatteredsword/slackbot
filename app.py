18965184cbf4b3554f64e2c17a5f005da05d6e55
Sean Mayo <sean.mayo@matsonfruit.com>
2021-09-17 16:33:10 -0700

    fixed bug

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import datetime
 import os
 import logging
 from slack_bolt import App
-#os.environ["SLACK_BOT_TOKEN"] = ""
-#os.environ["SIGNING_SECRET"] = ""
+
+# os.environ["SLACK_BOT_TOKEN"] = ""
+# os.environ["SIGNING_SECRET"] = ""
 
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SIGNING_SECRET")
+    signing_secret=os.environ.get("SIGNING_SECRET"),
 )
+
 
 @app.message(":wave:")
 def message_hello(message, say):
@@ -28,6 +30,7 @@ def message_hello(message, say):
         text=f"Hey there <@{message['user']}>!",
     )
 
+
 # The test_callback shortcut listens to a shortcut with the callback_id "test_callback"
 @app.shortcut("test_callback")
 def open_modal(ack, shortcut, client):
@@ -46,21 +49,22 @@ def open_modal(ack, shortcut, client):
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
-                    }
+                        "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>.",
+                    },
                 },
                 {
                     "type": "context",
                     "elements": [
                         {
                             "type": "mrkdwn",
-                            "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+                            "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>",
                         }
-                    ]
-                }
-            ]
-        }
+                    ],
+                },
+            ],
+        },
     )
+
 
 # The echo command simply echoes on command
 @app.command("/echo")
@@ -68,7 +72,7 @@ def repeat_text(ack, respond, command):
     # Acknowledge command request
     ack()
     respond(f"{command['text']}")
-l
+
 
 # Start your app
 if __name__ == "__main__":
